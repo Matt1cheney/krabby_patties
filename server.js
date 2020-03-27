5b790e880a1de9c4bfdf6d970db44335e79f8b46
Matthew Cheney <matt1cheney@gmail.com>
2020-03-26 20:44:53 -0400

    building scaffolding

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
